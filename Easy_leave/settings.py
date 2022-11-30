4897759cd774c87ad587f1831caa17968e8b1f67
Yugansh Goyal <yugansh.12011043@iiitsonepat.ac.in>
2022-11-30 16:06:46 +0530

    deployment error

--- Easy_leave/settings.py
+++ Easy_leave/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-1e3a.up.railway.app']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
